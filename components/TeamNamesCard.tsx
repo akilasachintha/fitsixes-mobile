b445cbf9d96a46f337d27d9cf446c0b285f6afc6
MilindiSenevirathne <milindianu@gmail.com>
2023-09-28 00:17:56 +0530

    added team names card component

---
+++ components/TeamNamesCard.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Image, ImageSourcePropType, StyleSheet, Text, View } from "react-native";
+import ImageHolder from "./ImageHolder";
+
+interface TeamNamesProps {
+    teamName1: string;
+    teamName2: string;
+    teamSource1: ImageSourcePropType;
+    teamSource2: ImageSourcePropType;
+}
+
+const TeamNamesCard: React.FC<TeamNamesProps> = ({ teamName1, teamName2, teamSource1, teamSource2 }) => {
+    return (
+        <View style={styles.cardContainer}>
+            <View style={styles.cardSecondContainer}>
+                <View style={styles.teamDetails}>
+                    <ImageHolder source={teamSource1} size={70} />
+                    <View style={styles.teamNameContainer}>
+                        <Text style={styles.teamName} numberOfLines={3} ellipsizeMode="tail">
+                            {teamName1.length > 20 ? `${teamName1.slice(0, 30)}...` : teamName1}
+                        </Text>
+                    </View>
+                </View>
+
+                <View style={styles.imageContainer}>
+                    <Image source={require('../assets/fit-sixes.png')} style={styles.image} />
+                </View>
+
+                <View style={styles.teamDetails}>
+                    <ImageHolder source={teamSource2} size={70} />
+                    <View style={styles.teamNameContainer}>
+                        <Text style={styles.teamName} numberOfLines={3} ellipsizeMode="tail">
+                            {teamName2.length > 20 ? `${teamName2.slice(0, 30)}...` : teamName2}
+                        </Text>
+                    </View>
+                </View>
+
+            </View>
+        </View>
+    )
+}
+
+export default TeamNamesCard;
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        alignSelf: 'center',
+        borderRadius: 32,
+        backgroundColor: "#101B62",
+        elevation: 5,
+        width: 320,
+        height: 150
+    },
+    cardSecondContainer: {
+        alignSelf: 'center',
+        padding: 25,
+        flexDirection: 'row'
+    },
+    teamDetails: {
+        flexDirection: 'column'
+    },
+    teamNameContainer: {
+        width: 70,
+    },
+    teamName: {
+        marginTop: 10,
+        color: '#fff',
+        fontSize: 11,
+        fontWeight: '700',
+        textAlign: 'center'
+    },
+    imageContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        bottom: 10
+    },
+    image: {
+        width: 130,
+        height: 130
+    }
+});
