3f574453f1e635d9eb54fa928034853e5ece6b4a
MilindiSenevirathne <milindianu@gmail.com>
2023-09-27 22:10:42 +0530

    fixed border radius & border width issue

--- components/ImageHolder.tsx
+++ components/ImageHolder.tsx
@@ -4,11 +4,28 @@ import { Image, ImageSourcePropType, StyleSheet, View } from "react-native";
 interface ImageHolderProps {
     source: ImageSourcePropType;
     size: number;
+    borderColor?: string;
+    borderWidth?: number;
 }
 
-const ImageHolder: React.FC<ImageHolderProps> = ({ source, size }) => {
+const ImageHolder: React.FC<ImageHolderProps> = ({
+    source,
+    size,
+    borderColor = "#13FAF8",
+    borderWidth = 2,
+}) => {
     return (
-        <View style={[styles.imageContainer, { width: size, height: size }]}>
+        <View
+            style={[
+                styles.imageContainer,
+                {
+                    width: size,
+                    height: size,
+                    borderColor,
+                    borderWidth,
+                },
+            ]}
+        >
             <Image source={source} style={styles.image} />
         </View>
     );
@@ -20,8 +37,6 @@ const styles = StyleSheet.create({
     imageContainer: {
         borderRadius: 50,
         overflow: "hidden",
-        borderColor: "#13FAF8",
-        borderWidth: 2
     },
     image: {
         flex: 1,
