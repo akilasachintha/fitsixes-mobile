b490df1b8ed174888a02311bcfadd8980c62c3f3
AyeshaniWeerasooriya <ayeshani@agrithmics.lk>
2023-09-29 15:10:58 +0530

    Added player's details component

--- components/PlayerDetailsCard.tsx
+++ components/PlayerDetailsCard.tsx
@@ -1,43 +1,101 @@
-import { Text, View, Image, StyleSheet } from 'react-native'
+import { Text, View, Image, StyleSheet, ImageSourcePropType } from 'react-native'
 import React, { Component } from 'react'
 import ImageHolder from './ImageHolder'
 
-export default class PlayerDetailsCard extends Component {
-    render() {
-        return (
-            <View
-                style={{
-                    width: 364,
-                    height: 75,
-                    backgroundColor: '#101B62',
-                    borderRadius: 112
-                }}
-            >
-                <View style={{ width: "100%", height: "100%", flexDirection: 'row', marginHorizontal: '3%' }}>
-                    <View style={{ width: '20%', marginVertical: 8, flexDirection: 'column' }}>
-                        <ImageHolder
-                            source={require('../assets/fit-sixes.png')} // Replace with the actual image path
-                            size={60} // Specify the desired size
-                            borderColor="#13FAF8" // Optionally, specify borderColor and borderWidth
-                            borderWidth={3}
-                        />
-                    </View>
-                    <View style={{ width: '40%', marginLeft: 15, marginVertical: 8, flexDirection: 'column', justifyContent: 'center' }}>
-                        <Text numberOfLines={1} style={{ color: 'white', fontSize: 18, fontWeight: 'bold', width: '100%', }}>
-                            Player name
-                        </Text>
-                        <Text numberOfLines={1} style={{ color: 'white', fontSize: 18, fontWeight: 'bold', width: '100%', }}>
-                            Player name
-                        </Text>
-                    </View>
-                    <View style={{ width: '20%', marginLeft: 15, marginVertical: 8, flexDirection: 'column', }}>
-                        <Image style={{ width: "100%", height: "100%" }} source={require('../assets/fit-sixes.png')} />
-                    </View>
 
-
-                </View>
-                {/* <Text style={{color:'green', fontSize:25}}>PlayerDetailsCard</Text> */}
-            </View>
-        )
-    }
+interface PlayerDetailsCardProps {
+    url: ImageSourcePropType;
+    playerName: String;
+    playerRole: String;
 }
+
+const PlayerDetailsCard: React.FC<PlayerDetailsCardProps> = ({
+    url,
+    playerName,
+    playerRole,
+
+}) => {
+    return (
+        <View style={styles.playerCardContainer}>
+            <View style={styles.playerCardSubContainer}>
+                <View style={styles.playerProfileContainer}>
+                    <ImageHolder
+                        source={url ? url : require('../assets/no-image.jpg')} // Replace with the actual image path
+                        size={60} // Specify the desired size
+                        borderColor="#13FAF8" // Optionally, specify borderColor and borderWidth
+                        borderWidth={3}
+                    />
+                </View>
+                <View style={styles.playerDetailsContainer}>
+                    <Text numberOfLines={1} style={styles.playerName}>
+                        {playerName}
+                    </Text>
+                    <Text numberOfLines={1} style={styles.playerName}>
+                        {playerRole}
+                    </Text>
+                </View>
+                <View style={styles.logoContainer}>
+                    <Image style={styles.logoView} resizeMode='center' source={require('../assets/images/fit-sixes-drawer-logo.png')} />
+                </View>
+            </View>
+        </View>
+    )
+
+}
+export default PlayerDetailsCard;
+
+const styles = StyleSheet.create({
+
+    playerCardContainer: {
+        width: 364,
+        height: 75,
+        backgroundColor: '#101B62',
+        borderRadius: 112,
+        alignSelf: 'center',
+    },
+
+    playerCardSubContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: '3%',
+    },
+
+    playerProfileContainer: {
+        width: '18%',
+        flexDirection: 'column',
+    },
+
+    playerDetailsContainer: {
+        width: '45%',
+        marginLeft: 15,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+
+    playerName: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '700',
+        width: '100%',
+        margin: 2
+    },
+
+    logoContainer: {
+        width: '30%',
+        marginLeft: 15,
+        flexDirection: 'column',
+        alignItems: 'flex-end'
+    },
+
+    logoView: {
+        width: "100%",
+        height: "100%",
+    },
+})
+
+
+
+
+
+
+
