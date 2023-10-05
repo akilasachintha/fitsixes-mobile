8e5105872a5a93c19a5d2676305e3f909c03cf78
Akila Sachintha <akilasachintha@gmail.com>
2023-10-06 00:21:46 +0530

    made some changes

--- screens/MatchesUpcomingScreen.tsx
+++ screens/MatchesUpcomingScreen.tsx
@@ -1,19 +1,22 @@
-import { SafeAreaView, ScrollView } from "react-native";
-import MatchDetailCard, { MatchStatus } from "../components/MatchDetailCard";
-import { PATHS } from "../config/paths";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import {SafeAreaView, ScrollView} from "react-native";
+import MatchDetailCard, {MatchStatus} from "../components/MatchDetailCard";
+import {PATHS} from "../config/paths";
+import {useEffect, useState} from "react";
+import {useAuth} from "../context/AuthContext";
+import {BASE_URL, createAxiosInstance} from "../config/axiosConfig";
 
 export default function MatchesUpcomingScreen() {
 
-    const [upcomingMatches, setUpcomingMatches] = useState([])
+    const [upcomingMatches, setUpcomingMatches] = useState([]);
+    const authContext = useAuth();
+    const axiosInstanceForFitSixes = createAxiosInstance(authContext, BASE_URL.FIT_SIXES);
 
     useEffect(() => {
-        let url = "https://j1kydf6tp3.execute-api.ap-south-1.amazonaws.com/dev/v1/matches/upcoming";
-        axios.get(`${url}`).then((response) => {
-            if (response.data && response.data.matches) {
-                setUpcomingMatches(response.data.matches.matches);
-                console.log(response.data, '----------data-----------------')
+        let url = "matches/ongoing";
+        axiosInstanceForFitSixes.get(`${url}`).then((response) => {
+            if (response && response.data && response.data.data && response.data.data.matches && response.data.data.matches.matches) {
+                setUpcomingMatches(response.data.data.matches.matches);
+                console.log(response.data, '----------data-----------------');
             } else {
                 console.error("Error");
             }
@@ -25,9 +28,10 @@ export default function MatchesUpcomingScreen() {
     return (
         <SafeAreaView>
             <ScrollView showsVerticalScrollIndicator={false}>
-                {upcomingMatches?.map((item: any) => {
+                {upcomingMatches && upcomingMatches.map((item: any, index) => {
                     return (
                         <MatchDetailCard
+                            key={index}
                             matchStatus={MatchStatus.Upcoming}
                             team1={item.team1}
                             team2={item.team2}
