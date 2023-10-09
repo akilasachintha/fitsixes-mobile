e7442f5d429580c5f116b9749b650c81c88eb497
Akila Sachintha <akilasachintha@gmail.com>
2023-10-10 02:16:59 +0530

    added socket

--- screens/MatchesLiveScreen.tsx
+++ screens/MatchesLiveScreen.tsx
@@ -33,7 +33,7 @@ export default function MatchesLiveScreen() {
 
     const connectToWebSocket = () => {
         ws.current = new WebSocket(
-            'ws://ec2-43-205-124-127.ap-south-1.compute.amazonaws.com:8080/socket.io/?EIO=4&transport=websocket',
+            'wss://free.blr2.piesocket.com/v3/1?api_key=oFGep6TlZyyDrDqwS8IktAIWWkzTwxpqUzmDNyaN&notify_self=1',
         );
 
         ws.current.onopen = () => {
@@ -85,24 +85,8 @@ export default function MatchesLiveScreen() {
                     serverMessages && serverMessages.length > 0 ? (
                         <View>
                             {serverMessages && serverMessages.length > 0 && serverMessages.map((item: any) => {
-                                let team1_score = `${item.team1.marks}/${item.team1.wickets}`
-                                let team2_score = `${item.team2.marks}/${item.team2.wickets}`
-                                let overs_T1 = `${item.team1.overs}.${item.team1.balls}`
-                                let overs_T2 = `${item.team2.overs}.${item.team2.balls}`
                                 return (
-                                    <MatchDetailCard
-                                        key={item.match_id}
-                                        matchStatus={MatchStatus.Live}
-                                        team1={item.team1.teamName}
-                                        team2={item.team2.teamName}
-                                        team1Score={team1_score}
-                                        team2Score={team2_score}
-                                        team1Image={PATHS.IMAGES.Team_1}
-                                        team2Image={PATHS.IMAGES.Team_2}
-                                        matchNo={item.match_id}
-                                        overs_T1={overs_T1}
-                                        overs_T2={overs_T2}
-                                    />
+                                    <Text>{JSON.stringify(item)}</Text>
                                 )
                             })}
                         </View>
