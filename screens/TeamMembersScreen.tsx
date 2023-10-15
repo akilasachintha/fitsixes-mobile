0d2ef8636a1c8dff812e33c9e0b757df1a83e4ff
Taniya Gangadari Senarathna <senarathnawtg.19@uom.lk>
2023-10-15 19:03:38 +0530

    Teams API

--- screens/TeamMembersScreen.tsx
+++ screens/TeamMembersScreen.tsx
@@ -1,10 +1,13 @@
 import PlayerDetailsCard from "@components/PlayerDetailsCard";
 import ImageHolder from "../components/ImageHolder";
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
+import {FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
 import {THEME} from "@constants/THEME";
 import {PATHS} from "@constants/PATHS";
+import { useEffect } from "react";
 
-export default function TeamMembersScreen() {
+export default function TeamMembersScreen(props : any) {
+    console.log("aaa",props.route.params.players)
+
     return (
         <SafeAreaView>
             <ScrollView showsVerticalScrollIndicator={false}>
@@ -12,10 +15,22 @@ export default function TeamMembersScreen() {
                     <ImageHolder source={PATHS.IMAGES.NO_IMAGE} size={70}/>
                     <Text style={styles.teamCardText}>Team Players</Text>
                 </View>
-                <PlayerDetailsCard url={PATHS.IMAGES.NO_IMAGE} playerName="Akila" playerRole="Batsman"/>
+                
+                {/* <PlayerDetailsCard url={PATHS.IMAGES.NO_IMAGE} playerName="Akila" playerRole="Batsman"/>
                 <PlayerDetailsCard url={PATHS.IMAGES.NO_IMAGE} playerName="Kamal" playerRole="Batsman"/>
-                <PlayerDetailsCard url={PATHS.IMAGES.NO_IMAGE} playerName="Isuru" playerRole="Batsman"/>
+                <PlayerDetailsCard url={PATHS.IMAGES.NO_IMAGE} playerName="Isuru" playerRole="Batsman"/> */}
             </ScrollView>
+            <FlatList
+                    showsVerticalScrollIndicator={false}
+                    style={styles.flatList}
+                    centerContent={true}
+                    numColumns={1}
+                    data={props.route.params.players}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({item, index}: { item: any; index: number }) =>
+                        <PlayerDetailsCard url={PATHS.IMAGES.NO_IMAGE} playerName={item.name} playerRole="Batsman"/>
+                }
+                />
         </SafeAreaView>
     )
 }
@@ -30,5 +45,8 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "bold",
         marginVertical: "4%",
-    }
+    },
+    flatList: {
+        // borderWidth:2
+    },
 });
